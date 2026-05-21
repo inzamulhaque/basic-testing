7628690bca0380de8ecb0969fcdb956e74634345
inzamulhaque <mdihalif@gmail.com>
2026-05-21 16:42:08 +0600

    test async fn

---
+++ async.js
@@ -0,0 +1,38 @@
+import cryptoJS from "crypto-js";
+
+export const encryptMessage = (message, key, callback) => {
+  const encryptedMessage = cryptoJS.AES.encrypt(message, key).toString();
+
+  callback(encryptedMessage);
+};
+
+export const encryptMessagePromise = (message, key) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const encryptedMessage = cryptoJS.AES.encrypt(message, key).toString();
+
+      if (encryptedMessage) {
+        resolve(encryptedMessage);
+      } else {
+        reject(new Error("Failed to encrypt message!"));
+      }
+    }, 2000);
+  });
+};
+
+const message = {
+  email: "explain@mail.com",
+  password: "1234567",
+};
+
+const secretKey = "sdffewuyvbhvsihiojijygugygf";
+
+encryptMessage(JSON.stringify(message), secretKey, (encryptedMessage) => {
+  console.log(encryptedMessage);
+});
+
+encryptMessagePromise(JSON.stringify(message), secretKey).then(
+  (encryptedMessage) => {
+    console.log(encryptedMessage);
+  },
+);
---
+++ async.test.js
@@ -0,0 +1,50 @@
+import { expect, it } from "vitest";
+import cryptoJS from "crypto-js";
+import { encryptMessage, encryptMessagePromise } from "./async";
+
+it("should encrypt a message", async () => {
+  const message = {
+    email: "explain@mail.com",
+    password: "1234567",
+  };
+
+  const secretKey = "sdffewuyvbhvsihiojijygugygf";
+
+  const encryptedData = await new Promise((resolve, reject) => {
+    encryptMessage(JSON.stringify(message), secretKey, (encryptedMessage) => {
+      resolve(encryptedMessage);
+    });
+  });
+
+  expect(encryptedData).toBeDefined();
+});
+
+it("should encrypt a message with promise", async () => {
+  const message = {
+    email: "explain@mail.com",
+    password: "1234567",
+  };
+
+  const secretKey = "sdffewuyvbhvsihiojijygugygf";
+
+  const encryptedData = await encryptMessagePromise(
+    JSON.stringify(message),
+    secretKey,
+  );
+
+  expect(encryptedData).toBeDefined();
+});
+
+it("should encrypt a message with promise and check is equal to message or not", async () => {
+  const message = "Test message";
+
+  const secretKey = "sdffewuyvbhvsihiojijygugygf";
+
+  const encryptedData = await encryptMessagePromise(message, secretKey);
+
+  const bytes = cryptoJS.AES.decrypt(encryptedData, secretKey);
+
+  const decryptData = bytes.toString(cryptoJS.enc.Utf8);
+
+  expect(decryptData).toBe(message);
+});
