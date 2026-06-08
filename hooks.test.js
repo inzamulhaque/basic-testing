206444f47df264af33cb7d51f83d44814839038a
inzamulhaque <mdihalif@gmail.com>
2026-06-08 19:56:26 +0600

    user before each

---
+++ hooks.test.js
@@ -0,0 +1,38 @@
+import { beforeEach, expect, it } from "vitest";
+import { User } from "./hooks";
+
+const testEmail = "test@test.com";
+
+let user;
+
+beforeEach(() => {
+  user = new User(testEmail);
+});
+
+it("should have an email property", () => {
+  expect(user).toHaveProperty("email");
+});
+
+it("should update the email", () => {
+  const newTestEmail = "newtest@test.com";
+
+  user.updateEmail(newTestEmail);
+
+  expect(user.email).toBe(newTestEmail);
+});
+
+it("should store the provided email value", () => {
+  expect(user.email).toBe(testEmail);
+});
+
+it("should clean the email", () => {
+  user.cleanEmail();
+
+  expect(user.email).toBe("");
+});
+
+it("should still have an email property after cleaning", () => {
+  user.cleanEmail();
+
+  expect(user).toHaveProperty("email");
+});
